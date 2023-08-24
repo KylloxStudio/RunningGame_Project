6f129f9bb0e1bd72ad56d556fa07a63a3bc76bda
Kyllox <kyllox4804@gmail.com>
2023-08-25 07:38:01 +0900

    Update

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,11 +7,10 @@ public class GameManager : MonoBehaviour
 {
     public Player player;
     public Enemy enemy;
-    public GameObject enemyGroup;
+    public List<Enemy> enemyGroup;
     public float spawnPercent;
 
     public Text scoreText;
-    public Text hpText;
     public GameObject gameoverPanel;
 
     public float timer = 0f;
@@ -20,7 +19,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -35,10 +34,12 @@ public class GameManager : MonoBehaviour
                 gameoverPanel.SetActive(true);
             }
             timer += Time.deltaTime;
+            scoreText.text = "Score: " + timer.ToString("N2");
             float random = Random.Range(0f, 100f);
             if (random <= spawnPercent)
             {
                 Enemy e = Instantiate(enemy);
+                enemyGroup.Add(e);
                 e.transform.position = new Vector3(Random.Range(10f, 15f), Random.Range(-1.5f, -3.3f));
             }
         }
